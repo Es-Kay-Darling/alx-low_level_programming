2692876ed4ae3e7c195f95a4ae6ce59d9eebe069
Es-Kay-Darling <eskayblay@gmail.com>
2022-04-11 19:53:01 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Retrun Always 0 ( Seccess/correct)]
+ */
+
+int mmain(void)
+{
+	char alphabet;
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alpahbet == 'q')
+		continue;
+
+	else if (alphabet == 'e')
+		continue;
+	putchar(alphabet);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+	
