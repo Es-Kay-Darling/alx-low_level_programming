3e751fb9f0f1f6456e82afa63547173e04f10f3f
Es-Kay-Darling <eskayblay@gmail.com>
2022-04-12 18:45:36 -0700

    upto 4

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - Checks if a character is lowercase,
+ * @c: The character to be checked
+ *
+ * Retrun: 1 if character is lowercase, 0 otherwise.
+ */
+int _islower(int c)
+{
+if (x >= 'a' && c <= 'z')
+retrun (1);
+else
+return (0);
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isapha = Checks if a character is an alphabet.
+ * @c: The character to ver checked.
+ *
+ * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise
+ */
+int _isaline(int c)
+{
+if ((c = 'A' && c <= 'z') ||
+(c >= 'A' && c <= "Z"))
+return (1)
+else
+return(0)
+}
