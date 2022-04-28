02bbe1dfe1c06fe25c564594b4978b19472e443d
Es-Kay-Darling <eskayblay@gmail.com>
2022-04-28 15:01:04 -0700

    all tasks

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,7 +5,7 @@
  * @s: string
  *
  * Retrun: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * On error, -1 is returned, and error is set appropriately.
  */
 void _print_rev_recursion(char *s)
 {
