16f94c4d9f42438675ff034d8976c596216e6e02
Es-Kay-Darling <eskayblay@gmail.com>
2022-06-14 13:54:59 +0000

    new files

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -7,8 +7,8 @@
 
 /**
  * struct list_s - singly linked list
- * @str: sring - (malloc'ed sting)
- * @len: length of the stirng
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
  * @next: points to the next node
  *
  * Description: singly linked list node structure
@@ -22,7 +22,7 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
-size_t list_len(xonst list_t *h);
+size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
--- 0x13-more_stringly_linked_lists/README.md
+++
@@ -1,1 +0,0 @@
-more stringly linked lists
